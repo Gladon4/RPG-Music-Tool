a2b6654f8b6195301405d60c09beb7ece48a7292
Gladon4 <mcgoldhand@gmail.com>
2022-01-21 13:35:20 +0100

    Skip and Stop Buttons, Spam Bug fixed 2

--- music-player.py
+++ music-player.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import ttk
 import pygame
 import random
+import time
 
 moods = {}
 # paths = ["CK3/", "Pillars_of_Eternity/"]
@@ -17,6 +18,9 @@ song_path = ""
 song_timer = 0
 time_left = 0
 paused = False
+timer = time.time()
+new_timer = timer
+running = False
 
 pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.mixer.init()
@@ -27,24 +31,34 @@ def update():
     global song_timer
     global paused
     global current_mood
+    global timer
+    global new_timer
+    global running
 
-    if not song == NONE:
+    running = True
+
+    if not paused and not song == NONE:
         song_progress['value'] = (song_timer - time_left) / song_timer * 100
         pygame.mixer.Sound.set_volume(song, volume.get() / 100)
 
-    if not paused and not song == NONE:
-        time_left -= 10
-        if time_left <= 100:
+        time_left -= (timer - new_timer)
+        new_timer = timer
+        timer = time.time()
+
+        if time_left <= 0.1:
             get_next_song(current_mood)
 
-    if time_left > 100:
-        label_current_song.after(10, update)
+    label_update.after(10, update)
 
 
 def get_next_song(mood):
     global song
     global song_path
     global current_mood
+    global paused
+
+    paused = False
+
     if not song == NONE:
         pygame.mixer.stop()
         song_id = random.randint(0, len(moods[mood]) - 1)
@@ -64,28 +78,51 @@ def play():
     global song_path
     global time_left
     global song_timer
+    global timer
+    global new_timer
 
     song = pygame.mixer.Sound(song_path)
     pygame.mixer.Sound.play(song)
-    label_current_song.config(text=song_path)
+    top_label = current_mood + "\n" + song_path
+    label_current_song.config(text=top_label)
 
-    song_timer = pygame.mixer.Sound.get_length(song) * 1000
+    song_timer = pygame.mixer.Sound.get_length(song)
     time_left = song_timer
-    update()
+    timer = time.time()
+    new_timer = timer
 
 
 def pause():
     global paused
+    global timer
+    global new_timer
 
-    paused = True
-    pygame.mixer.pause()
+    if paused:
+        timer = time.time()
+        new_timer = timer
+        paused = False
+        pygame.mixer.unpause()
+
+    else:
+        paused = True
+        pygame.mixer.pause()
 
 
-def unpause():
+def stop():
     global paused
+    global song
 
-    paused = False
-    pygame.mixer.unpause()
+    pygame.mixer.stop()
+    song = NONE
+    paused = True
+    song_progress['value'] = 0
+    label_current_song.config(text="")
+
+
+def skip():
+    global current_mood
+
+    get_next_song(current_mood)
 
 
 for path in paths:
@@ -117,29 +154,35 @@ status.grid(row=3, column=0)
 
 i = 0
 for mood in moods:
+    text = mood + "\n" + str(len(moods[mood]))
     mood_play_button = Button(
-        buttons, text=mood, command=lambda mood=mood: get_next_song(mood), height=5, width=10)
+        buttons, text=text, command=lambda mood=mood: get_next_song(mood), height=5, width=10)
     mood_play_button.grid(row=i//6+1, column=i % 6)
     i += 1
 
+label_update = Label(root, text="")
+if not running:
+    label_update.after(10, update)
 
 label_current_song = Label(root, text="")
 label_current_song.grid(row=0, column=0)
 
 song_progress = ttk.Progressbar(
     status, orient=HORIZONTAL, length=400, mode='determinate')
-song_progress.grid(row=0, column=2)
+song_progress.grid(row=0, column=3)
 
 volume = Scale(status, from_=100, to=0, length=120, width=25)
 volume.set(10)
-volume.grid(row=0, column=3)
+volume.grid(row=0, column=4)
 
+button_stop = Button(status, text="Stop", command=stop)
+button_stop.grid(row=0, column=0)
 
-button_pause = Button(status, text="Pause", command=pause)
-button_pause.grid(row=0, column=1)
+button_pause_unpause = Button(status, text="Play/Pause", command=pause)
+button_pause_unpause.grid(row=0, column=1)
 
-button_unpause = Button(status, text="Play", command=unpause)
-button_unpause.grid(row=0, column=0)
+button_skip = Button(status, text="Skip", command=skip)
+button_skip.grid(row=0, column=2)
 
 col_count, row_count = root.grid_size()
 
@@ -148,5 +191,6 @@ for col in range(col_count):
 
 for row in range(row_count):
     root.grid_rowconfigure(row, minsize=100)
+
 
 root.mainloop()
