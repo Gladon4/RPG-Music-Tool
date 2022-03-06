fda444b8c472438d9faf2e5104ca00b544a1478a
Gladon4 <alex.mario.grille.amg@gmail.com>
2022-03-06 16:23:11 +0100

    Version 0.2 Rewrite Complete Rewrite of music-player.py (Cleanup and Performance) Button Images

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# RPG-Music-Tool (ver. 0.1)
+# RPG-Music-Tool (ver. 0.2)
 
 A small music tool for TTRPGs.
 
-This Tool allows you to play music based on the current mood or setting for your TableTop RolePlayingGames. 
-This Programm uses local music files, instead of for example a Spotify Playlist. So you can always use it as long as you have your music with you.
+This Tool allows you to play music based on the current mood or setting for your TableTop RolePlayingGames.
+This Program uses local music files, instead of for example a Spotify Playlist. So you can always use it as long as you have your music with you.
 
 ## Python Dependencies (Use pip to install):
 
@@ -13,17 +13,21 @@ tkinter
 
 ttk
 
+mutagen
+
 This is python, so just install python3 and libraries and it should work.
 
 ## How to set up:
 ### 1.
+
+Supports Windows and Linux as of now!
 
 Download the repository, either as a zip and unzip it, or with git clone.
 
 ### 2.
 
 In the directory of your songs create a songs.csv, which lists all songs (with file ending) you want to include.
-Seperate them by line breaks and add the fitting moods after like this:
+Separate them by line breaks and add the fitting moods after like this:
 
 Combat A.mp3,Combat;Tense\
 Combat B.mp3,Combat;War\
@@ -32,7 +36,7 @@ Tavern A.mp3,Tavern;Festive
 
 ### 3.
 
-Create a paths.csv (or rename the example_paths.csv) in the directory of the programm, and put in the paths to the music direcories, seperate with comma (,). 
+Create a paths.csv (or rename the example_paths.csv) in the directory of the program, and put in the paths to the music directories, separate with comma (,).
 
 Example for Linux:
 
@@ -48,13 +52,12 @@ That's it.
 ## How to Use it:
 
 The Programm should display all created moods and how many songs are associated with that mood.
-Just click on the mood you currently need and it should start playing a random sequence of songs with the choosen mood. 
+Just click on the mood you currently need and it should start playing a random sequence of songs with the chosen mood.
 (This is suited well for touch displays, if you have one)
 
 ![image](https://user-images.githubusercontent.com/58821835/150589364-aa6d69b8-9553-4cf4-a975-2c6a8ad0c77c.png)
 
 You can also Stop or Pause the playback, or skip the current song.
-You can see how much time is remaining and you can controll the volume.
+You can see how much time is remaining and you can control the volume.
 
 ![image](https://user-images.githubusercontent.com/58821835/150590072-74710a3b-5efa-48ee-9f50-7d0840e375e9.png)
-
--- music-player.py
+++ music-player.py
@@ -1,145 +1,142 @@
-import csv
-from tkinter import *
-from tkinter import ttk
-import pygame
-import random
-import time
+# --- IMPORT --- #
+from mutagen.mp3 import MP3
 import os
+import time
+import random
+import pygame
+from tkinter import ttk
+from tkinter import *
+import csv
 
-moods = {}
 
-base_path = os.path.dirname(__file__)
-with open(base_path+"/paths.csv", "r", encoding="utf-8-sig") as csv_file:
-    list = csv.reader(csv_file, delimiter=',', quotechar='|')
-    for row in list:
-        paths = row
+# --- CONSTANTS --- #
+BASE_PATH = os.path.dirname(__file__)
 
-current_mood = NONE
-song = NONE
-song_path = ""
-song_timer = 0
-time_left = 0
+# --- Variables --- #
+global theme
+global length
+global stopped
+
+themes = {}
+first = True
 paused = False
-timer = time.time()
-new_timer = timer
-running = False
-
-pygame.mixer.pre_init(44100, -16, 2, 512)
-pygame.mixer.init()
+stopped = False
 
 
-def update():
-    global time_left
-    global song_timer
-    global paused
-    global current_mood
-    global timer
-    global new_timer
-    global running
-
-    running = True
-
-    if not paused and not song == NONE:
-        song_progress['value'] = (song_timer - time_left) / song_timer * 100
-        pygame.mixer.Sound.set_volume(song, volume.get() / 100)
-
-        time_left -= (timer - new_timer)
-        new_timer = timer
-        timer = time.time()
-
-        if time_left <= 0.1:
-            get_next_song(current_mood)
-
-    label_update.after(10, update)
-
-
-def get_next_song(mood):
-    global song
-    global song_path
-    global current_mood
+# --- Functions --- #
+def change_theme(next_theme):
+    global theme
+    global stopped
     global paused
 
+    stopped = False
     paused = False
+    theme = next_theme
+    next_song = get_next_song(next_theme)
 
-    if not song == NONE:
-        pygame.mixer.stop()
-        song_id = random.randint(0, len(moods[mood]) - 1)
-        current_mood = mood
-        song_path = moods[mood][song_id]
-        play()
+    label_current_theme.config(text=next_theme)
+    button_pause.config(image=pause_image)
 
-    else:
-        song_id = random.randint(0, len(moods[mood]) - 1)
-        current_mood = mood
-        song_path = moods[mood][song_id]
-        play()
+    play(next_song)
 
 
-def play():
-    global song
-    global song_path
-    global time_left
-    global song_timer
-    global timer
-    global new_timer
+def play(song):
+    global length
+    global first
 
-    song = pygame.mixer.Sound(song_path)
-    pygame.mixer.Sound.play(song)
-    top_label = current_mood + "\n" + song_path
-    label_current_song.config(text=top_label)
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play(loops=0)
+    song_mut = MP3(song)
+    length = song_mut.info.length
 
-    song_timer = pygame.mixer.Sound.get_length(song)
-    time_left = song_timer
-    timer = time.time()
-    new_timer = timer
+    if BASE_PATH[1] == ":": # Windows
+        song_name = song.split("\\")[len(song.split("\\")) - 1].split(".mp3")[0]
+        label_current_song.config(text=song_name)
+
+    else: # Linux
+        song_name = song.split("/")[len(song.split("/")) - 1].split(".mp3")[0]
+        label_current_song.config(text=song_name)
+
+    if first:
+        first = False
+        song_duration()
+
+
+def get_next_song(next_theme):
+    song_id = random.randint(0, len(themes[next_theme]) - 1)
+    song_path = themes[next_theme][song_id]
+    return song_path
+
+
+def song_duration():
+    if not stopped:
+        current_time = pygame.mixer.music.get_pos() / 1000
+        converted_current_time = time.strftime('%M:%S', time.gmtime(current_time))
+        converted_length = time.strftime('%M:%S', time.gmtime(length))
+        song_time = str(converted_current_time) + " / " + str(converted_length)
+
+        label_duration_song.config(text=song_time)
+        song_progress['value'] = current_time / length * 100
+
+        if current_time <= 0 and not stopped:
+            song = get_next_song(theme)
+            play(song)
+
+    label_duration_song.after(1000, song_duration)
 
 
 def pause():
     global paused
-    global timer
-    global new_timer
-
     if paused:
-        timer = time.time()
-        new_timer = timer
         paused = False
-        pygame.mixer.unpause()
-
+        button_pause.config(image=pause_image)
+        pygame.mixer.music.unpause()
     else:
         paused = True
-        pygame.mixer.pause()
+        button_pause.config(image=play_image)
+        pygame.mixer.music.pause()
 
 
 def stop():
-    global paused
-    global song
+    global stopped
 
-    pygame.mixer.stop()
-    song = NONE
-    paused = True
-    song_progress['value'] = 0
-    label_current_song.config(text="")
+    stopped = True
+    pygame.mixer.music.stop()
+
+    label_current_song.config(text="No Song Playing")
+    label_current_theme.config(text="No Theme Selected")
+    song_time = "00:00 / 00:00"
+    label_duration_song.config(text=song_time)
 
 
 def skip():
-    global current_mood
+    if not stopped:
+        song = get_next_song(theme)
+        play(song)
 
-    get_next_song(current_mood)
+def volume(pos):
+    pygame.mixer.music.set_volume(float(pos) / 100)
+    volume_frame.config(text=str(int(float(pos))))
 
+
+# --- INIT --- #
+with open(BASE_PATH+"/paths.csv", "r", encoding="utf-8-sig") as csv_file:
+    list = csv.reader(csv_file, delimiter=',', quotechar='|')
+    for row in list:
+        paths = row
 
 for path in paths:
     with open(path + "/songs.csv", "r", encoding="utf-8-sig") as csv_file:
         list = csv.reader(csv_file, delimiter=',', quotechar='|')
         for row in list:
-            moods_of_song = row[1].split(";")
-            for mood in moods_of_song:
-                if not mood == "":
-                    if mood in moods:
-                        moods[mood] += [path + row[0]]
+            for theme in row[1].split(";"):
+                if not theme == "":
+                    if theme in themes:
+                        themes[theme] += [path + row[0]]
                     else:
-                        moods[mood] = [path + row[0]]
-
-# print(moods)
+                        themes[theme] = [path + row[0]]
+pygame.mixer.init()
+pygame.mixer.music.set_volume(0.05)
 
 root = Tk()
 root.title("RPG Music Tool")
@@ -148,51 +145,53 @@ root.geometry("800x600")
 root.grid_rowconfigure(0, weight=0)
 root.grid_columnconfigure(0, weight=1)
 
-
-buttons = LabelFrame(root, text="", pady=15, padx=25)
+# LabelFrames
+buttons = LabelFrame(root, text="", pady=15, padx=15)
 buttons.grid(row=1, column=0)
-status = LabelFrame(root, text="", pady=15, padx=15)
-status.grid(row=3, column=0)
+lower_frame = LabelFrame(root, text="", pady=5, padx=15)
+lower_frame.grid(row=3, column=0)
+status = LabelFrame(lower_frame, text="", pady=15, padx=15)
+status.grid(row=0, column=0, padx=5)
+volume_frame = LabelFrame(lower_frame, text="3", pady=15, padx=15)
+volume_frame.grid(row=0,column=1, padx=5)
 
-i = 0
-for mood in moods:
-    text = mood + "\n" + str(len(moods[mood]))
-    mood_play_button = Button(
-        buttons, text=text, command=lambda mood=mood: get_next_song(mood), height=5, width=10)
-    mood_play_button.grid(row=i//6+1, column=i % 6)
-    i += 1
+#Labels
+label_current_song = Label(root, text="No Song Playing")
+label_current_song.grid(row=2, column=0, pady=5)
 
-label_update = Label(root, text="")
-if not running:
-    label_update.after(10, update)
+label_current_theme = Label(root, text="No Theme Selected")
+label_current_theme.grid(row=0, column=0, pady=5)
 
-label_current_song = Label(root, text="")
-label_current_song.grid(row=0, column=0)
+label_duration_song = Label(status, text="00:00 / 00:00")
+label_duration_song.grid(row=0, column=4, padx=5)
 
-song_progress = ttk.Progressbar(
-    status, orient=HORIZONTAL, length=400, mode='determinate')
-song_progress.grid(row=0, column=3)
+song_progress = ttk.Progressbar(status, orient=HORIZONTAL, length=300, mode='determinate')
+song_progress.grid(row=0, column=3, padx=5)
 
-volume = Scale(status, from_=100, to=0, length=120, width=25)
-volume.set(10)
-volume.grid(row=0, column=4)
+stop_image = PhotoImage(file="img/stop_img.png")
+skip_image = PhotoImage(file="img/skip_img.png")
+pause_image = PhotoImage(file="img/pause_img.png")
+play_image = PhotoImage(file="img/play_img.png")
 
-button_stop = Button(status, text="Stop", command=stop)
+# Inputs
+button_stop = Button(status, text="", command=stop, pady=15, padx=15, image=stop_image)
 button_stop.grid(row=0, column=0)
 
-button_pause_unpause = Button(status, text="Play/Pause", command=pause)
-button_pause_unpause.grid(row=0, column=1)
+button_pause = Button(status, text="", command=pause, pady=15, padx=15, image=pause_image)
+button_pause.grid(row=0, column=1)
 
-button_skip = Button(status, text="Skip", command=skip)
+button_skip = Button(status, text="", command=skip, pady=15, padx=15, image=skip_image)
 button_skip.grid(row=0, column=2)
 
-col_count, row_count = root.grid_size()
+volume_changer = ttk.Scale(volume_frame, from_=100, to=0, orient=VERTICAL, value=3, command=volume, length=120)
+volume_changer.pack()
 
-for col in range(col_count):
-    root.grid_columnconfigure(col, minsize=100)
+i = 0
+for theme in themes:
+    text = theme + "\n" + str(len(themes[theme]))
+    theme_play_button = Button(buttons, text=text, command=lambda theme=theme: change_theme(theme), height=5, width=10)
+    theme_play_button.grid(row=i//6+1, column=i % 6)
+    i += 1
 
-for row in range(row_count):
-    root.grid_rowconfigure(row, minsize=100)
-
-
+# --- TK Mainloop --- #
 root.mainloop()
